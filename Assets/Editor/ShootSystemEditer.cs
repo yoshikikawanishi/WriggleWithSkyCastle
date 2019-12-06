3ba7f7927158e57a88cc42ad6284450c54fa8231
yoshikikawanishi <yoshikikawanishi449@gmail.com>
2019-12-06 14:32:04 +0900

    12/6
    ShootSystem

--- Assets/Editor/ShootSystemEditer.cs
+++ Assets/Editor/ShootSystemEditer.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+/// <summary>
+/// ShootSystemのインスペクタ表示の設定
+/// </summary>
 [CustomEditor(typeof(ShootSystem))]
 public class ShootSystemEditer : Editor {
 
     public override void OnInspectorGUI() {
         ShootSystem obj = target as ShootSystem;
         //基本設定
+        obj.play_On_Awake = EditorGUILayout.Toggle("PlayOnAwake", obj.play_On_Awake);
         obj.bullet = (GameObject)EditorGUILayout.ObjectField("Bullet", obj.bullet, typeof(GameObject), false);
-        obj.parent = (GameObject)EditorGUILayout.ObjectField("Parent", obj.parent, typeof(GameObject), true);      
+        obj.parent = (Transform)EditorGUILayout.ObjectField("Parent", obj.parent, typeof(Transform), true);
+        obj.lifeTime = EditorGUILayout.FloatField("LifeTime", obj.lifeTime);        
+        obj.max_Speed = EditorGUILayout.FloatField("MaxSpeed", obj.max_Speed);
 
         EditorGUILayout.Space();
 
@@ -25,49 +31,71 @@ public class ShootSystemEditer : Editor {
                     obj.inter_Angle_Deg = EditorGUILayout.FloatField("InterAngle_Deg", obj.inter_Angle_Deg);
                     obj.count = EditorGUILayout.IntField("Count", obj.count);
                     obj.span = EditorGUILayout.FloatField("Span", obj.span);
+                    obj.is_Count_Up_Param = EditorGUILayout.Toggle("CountUpParam", obj.is_Count_Up_Param);
                     break;
+
                 case ShootSystem.KIND.Even:
                     obj.num = EditorGUILayout.IntField("Num", obj.num);
                     obj.inter_Angle_Deg = EditorGUILayout.FloatField("InterAngle_Deg", obj.inter_Angle_Deg);
                     obj.count = EditorGUILayout.IntField("Count", obj.count);
                     obj.span = EditorGUILayout.FloatField("Span", obj.span);
+                    obj.is_Count_Up_Param = EditorGUILayout.Toggle("CountUpParam", obj.is_Count_Up_Param);
                     break;
+
                 case ShootSystem.KIND.Diffusion:
                     obj.num = EditorGUILayout.IntField("Num", obj.num);
                     obj.center_Angle_Deg = EditorGUILayout.FloatField("CenterAngle_Deg", obj.center_Angle_Deg);
                     obj.count = EditorGUILayout.IntField("Count", obj.count);
                     obj.span = EditorGUILayout.FloatField("Span", obj.span);
+                    obj.is_Count_Up_Param = EditorGUILayout.Toggle("CountUpParam", obj.is_Count_Up_Param);
                     break;
+
                 case ShootSystem.KIND.nWay:
                     obj.num = EditorGUILayout.IntField("Num", obj.num);
                     obj.inter_Angle_Deg = EditorGUILayout.FloatField("InterAngle_Deg", obj.inter_Angle_Deg);
                     obj.center_Angle_Deg = EditorGUILayout.FloatField("CenterAngle_Deg", obj.center_Angle_Deg);
                     obj.count = EditorGUILayout.IntField("Count", obj.count);
                     obj.span = EditorGUILayout.FloatField("Span", obj.span);
+                    obj.is_Count_Up_Param = EditorGUILayout.Toggle("CountUpParam", obj.is_Count_Up_Param);
                     break;
+
                 case ShootSystem.KIND.Scatter:
                     obj.shoot_Rate = EditorGUILayout.FloatField("Rate", obj.shoot_Rate);
                     obj.center_Angle_Deg = EditorGUILayout.FloatField("CenterAngle_Deg", obj.center_Angle_Deg);
                     obj.arc_Deg = EditorGUILayout.FloatField("Arc", obj.arc_Deg);
                     obj.duration = EditorGUILayout.FloatField("Duration", obj.duration);
+                    obj.is_Count_Up_Param = false;
                     break;
+
                 case ShootSystem.KIND.Spiral:
                     obj.shoot_Rate = EditorGUILayout.FloatField("Rate", obj.shoot_Rate);
                     obj.inter_Angle_Deg = EditorGUILayout.FloatField("InterAngle_Deg", obj.inter_Angle_Deg);
+                    obj.center_Angle_Deg = EditorGUILayout.FloatField("CenterAngle_Deg", obj.center_Angle_Deg);
                     obj.duration = EditorGUILayout.FloatField("Duration", obj.duration);
+                    obj.is_Count_Up_Param = false;
                     break;
             }
-
-        }
+                        
+            if (obj.is_Count_Up_Param) {
+                obj.num_Up = EditorGUILayout.IntField("Num", obj.num_Up);
+                obj.speed_Up = EditorGUILayout.FloatField("Speed", obj.speed_Up);
+                obj.inter_Angle_Up = EditorGUILayout.FloatField("InterAngle", obj.inter_Angle_Up);
+                obj.center_Angle_Up = EditorGUILayout.FloatField("CenterAngle", obj.center_Angle_Up);
+            }
+        }       
 
         EditorGUILayout.Space();
-
-        //速度
-        obj.max_Speed = EditorGUILayout.FloatField("MaxSpeed", obj.max_Speed);
-        obj.velocity_Over_Lifetime = EditorGUILayout.Foldout(obj.velocity_Over_Lifetime, "VelocityOverTime");
-        if (obj.velocity_Over_Lifetime == true) {
+    
+        //加速度
+        obj.is_Acceleration = EditorGUILayout.Toggle("VelocityOverTime", obj.is_Acceleration);        
+        if (obj.is_Acceleration) {
+            if(obj.velocity_Forward == null) {
+                obj.velocity_Forward = AnimationCurve.Linear(0, 0, obj.lifeTime, 1.0f);
+                obj.velocity_Lateral = AnimationCurve.Linear(0, 0, obj.lifeTime, 1.0f);
+            }
             obj.velocity_Forward = EditorGUILayout.CurveField("Forward", obj.velocity_Forward);
-            obj.velocity_Lateral = EditorGUILayout.CurveField("Lateral", obj.velocity_Lateral);
+            obj.velocity_Lateral = EditorGUILayout.CurveField("Lateral", obj.velocity_Lateral);          
         }
     }    
+    
 }
--- Assets/Scripts/Function/ShootFunction/ShootSystem.cs
+++ Assets/Scripts/Function/ShootFunction/ShootSystem.cs
@@ -16,18 +16,19 @@ public class ShootSystem : MonoBehaviour {
     public KIND kind;
 
     //基本設定
+    public bool play_On_Awake = false;
     public GameObject bullet = null;
-    public GameObject parent = null;
+    public Transform parent = null;
     public float max_Speed = 100f;
+    public float lifeTime = 5;
 
-    public bool velocity_Over_Lifetime = false;
+    public bool is_Acceleration = false;
     public AnimationCurve velocity_Forward;
     public AnimationCurve velocity_Lateral;
 
-    public float lifeTime = 5;
 
     //その他設定
-    public bool other_Param = true;
+    public bool other_Param = true;    
     // 1:奇数段 / 2:偶数弾 / 3:全方位弾 / 4:nWay弾 / 5:ばらまき弾 / 6:渦巻き弾
     public int num = 1;                 //1, 2, 3, 4
     public int count = 5;               //1, 2, 3, 4
@@ -38,8 +39,21 @@ public class ShootSystem : MonoBehaviour {
     public float shoot_Rate = 30f;      //5, 6  
     public float duration = 5.0f;       //5, 6
 
+    //繰り返し時、値の変更用
+    public bool is_Count_Up_Param;
+    public int num_Up = 0;
+    public float speed_Up = 0;
+    public float inter_Angle_Up = 0;
+    public float center_Angle_Up = 0;
+
     private ObjectPool bullet_Pool; //弾
 
+
+    private void Start() {
+        if (play_On_Awake) {
+            Shoot();
+        }
+    }
 
     /// <summary>
     /// インスペクターで設定したショットを打つ
@@ -47,9 +61,9 @@ public class ShootSystem : MonoBehaviour {
     public void Shoot() {
         bullet_Pool = ObjectPoolManager.Instance.Get_Pool(bullet);
         switch (kind) {
-            case KIND.Scatter: StartCoroutine("Scatter_Shoot"); break;
-            case KIND.Spiral: StartCoroutine("Spiral_Shoot"); break;
-            default: StartCoroutine("Shoot_Cor"); break;
+            case KIND.Scatter:  StartCoroutine("Scatter_Shoot"); break;
+            case KIND.Spiral:   StartCoroutine("Spiral_Shoot"); break;
+            default:            StartCoroutine("Shoot_Cor");    break;
         }
     }
     
@@ -58,19 +72,37 @@ public class ShootSystem : MonoBehaviour {
     /// ショットを止める
     /// </summary>
     public void Stop_Shoot() {
-        StopAllCoroutines();
+        switch (kind) {
+            case KIND.Scatter:  StopCoroutine("Scatter_Shoot"); break;
+            case KIND.Spiral:   StopCoroutine("Spiral_Shoot");  break;
+            default:            StopCoroutine("Shoot_Cor");     break;
+        }
     }
 
 
     //ショット用のコルーチン
     private IEnumerator Shoot_Cor() {
+        List<GameObject> bullet_List = new List<GameObject>();
+
         for (int i = 0; i < count; i++) {
             switch (kind) {
-                case KIND.Odd: Odd_Num_Shoot(); break;
-                case KIND.Even: Even_Num_Shoot(); break;
-                case KIND.Diffusion: Diffusion_Shoot(); break;
-                case KIND.nWay: nWay_Shoot(); break;
+                case KIND.Odd:          bullet_List = Odd_Num_Shoot();  break;
+                case KIND.Even:         bullet_List = Even_Num_Shoot(); break;
+                case KIND.Diffusion:    bullet_List = Diffusion_Shoot(); break;
+                case KIND.nWay:         bullet_List = nWay_Shoot();     break;
             }
+            //弾の加速
+            if (is_Acceleration) {
+                StartCoroutine(Accelerat_Bullet_Cor(bullet_List, max_Speed));
+            }
+            //パラメータの変更
+            if (is_Count_Up_Param) {
+                num += num_Up;
+                max_Speed += speed_Up;
+                inter_Angle_Deg += inter_Angle_Up;
+                center_Angle_Deg += center_Angle_Up;
+            }
+
             yield return new WaitForSeconds(span);
         }
     }
@@ -82,9 +114,10 @@ public class ShootSystem : MonoBehaviour {
     /// <returns></returns>
     private IEnumerator Scatter_Shoot() {
         float angle = center_Angle_Deg;
-        for(float t = 0; t < duration; t += Time.deltaTime) {
+        for(float t = 0; t < duration; t += 1 / shoot_Rate) {
             angle = center_Angle_Deg + Random.Range(-arc_Deg / 2, arc_Deg / 2);
             var b = Turn_Shoot_Bullet(angle);
+            StartCoroutine(Accelerate_Bullet_Cor(b, max_Speed));
             yield return new WaitForSeconds(1 / shoot_Rate);
         }
     }
@@ -96,8 +129,9 @@ public class ShootSystem : MonoBehaviour {
     /// <returns></returns>
     private IEnumerator Spiral_Shoot() {
         float angle = center_Angle_Deg;
-        for(float t = 0; t < duration; t += Time.deltaTime) {
+        for(float t = 0; t < duration; t += 1 / shoot_Rate) {
             var b = Turn_Shoot_Bullet(angle);
+            StartCoroutine(Accelerate_Bullet_Cor(b, max_Speed));
             angle += inter_Angle_Deg;
             yield return new WaitForSeconds(1 / shoot_Rate);
         }
@@ -126,7 +160,7 @@ public class ShootSystem : MonoBehaviour {
         for (int i = 0; i < num; i++) {
             var b = Turn_Shoot_Bullet(angle);
             bullet_List.Add(b);
-            angle += inter_Angle_Deg * i;
+            angle += inter_Angle_Deg;
         }
 
         return bullet_List;
@@ -152,7 +186,7 @@ public class ShootSystem : MonoBehaviour {
         for (int i = 0; i < num; i++) {
             var b = Turn_Shoot_Bullet(angle);
             bullet_List.Add(b);
-            angle += inter_Angle_Deg * i;
+            angle += inter_Angle_Deg;
         }
 
         return bullet_List;
@@ -207,9 +241,11 @@ public class ShootSystem : MonoBehaviour {
     /// </summary>
     public GameObject Turn_Shoot_Bullet(float angle_Deg) {
         var turn_Bullet = bullet_Pool.GetObject();                      //生成
-        turn_Bullet.transform.SetParent(parent.transform);              //親オブジェクト        
+        turn_Bullet.transform.SetParent(parent);                        //親オブジェクト        
         turn_Bullet.transform.position = transform.position;            //座標
+        turn_Bullet.transform.rotation = new Quaternion(0, 0, 0, 0);
         turn_Bullet.transform.Rotate(new Vector3(0, 0, 1), angle_Deg);  //回転
+
         turn_Bullet.GetComponent<Rigidbody2D>().velocity = turn_Bullet.transform.right * max_Speed;     //初速
         if (lifeTime > 0) {
             Delete_Bullet(turn_Bullet, lifeTime);                       //寿命
@@ -228,4 +264,57 @@ public class ShootSystem : MonoBehaviour {
             bullet.AddComponent<Bullet>().Set_Inactive(lifeTime);
         }
     }    
+
+
+    //弾の加速(単体)
+    public IEnumerator Accelerate_Bullet_Cor(GameObject bullet, float max_Speed) {
+        Rigidbody2D bullet_Rigid = bullet.GetComponent<Rigidbody2D>();        
+        
+        float forward = max_Speed;
+        float lateral = 0;
+        for(float t = 0; t < lifeTime; t += Time.deltaTime) {
+            if(bullet_Rigid == null) {
+                yield break;
+            }
+            forward = max_Speed * velocity_Forward.Evaluate(t);                 //前方向
+            lateral = max_Speed * velocity_Lateral.Evaluate(t);                 //横方向
+            bullet_Rigid.velocity = bullet.transform.right * forward + bullet.transform.up * lateral;     //速度代入
+            float dirVelocity = Mathf.Atan2(bullet_Rigid.velocity.y, bullet_Rigid.velocity.x) * Mathf.Rad2Deg;    //進行方向に回転
+            bullet.transform.rotation = Quaternion.AngleAxis(dirVelocity, new Vector3(0, 0, 1));
+            yield return new WaitForSeconds(0.016f);
+        }
+    }
+
+
+    //弾の加速(List)
+    public IEnumerator Accelerat_Bullet_Cor(List<GameObject> bullet_List, float max_Speed) {
+        if (bullet_List[0].GetComponent<Rigidbody2D>() == null) {
+            Debug.Log("Bullet Must Attached Rigidbody2D");
+            yield break;
+        }
+
+        //Rigidbody2Dの取得
+        List<Rigidbody2D> rigid_List = new List<Rigidbody2D>();
+        for (int i = 0; i < bullet_List.Count; i++) {
+            rigid_List.Add(bullet_List[i].GetComponent<Rigidbody2D>());
+        }
+
+        //速度変更
+        float forward = max_Speed;
+        float lateral = 0;
+        for (float t = 0; t < lifeTime; t += Time.deltaTime) {
+            for (int i = 0; i < rigid_List.Count; i++) {
+                if (rigid_List[i] == null) {
+                    continue;
+                }                
+                forward = max_Speed * velocity_Forward.Evaluate(t); //前方向
+                lateral = max_Speed * velocity_Lateral.Evaluate(t); //横方向                
+                rigid_List[i].velocity = bullet_List[i].transform.right * forward + bullet_List[i].transform.up * lateral;    //速度代入
+                float dirVelocity = Mathf.Atan2(rigid_List[i].velocity.y, rigid_List[i].velocity.x) * Mathf.Rad2Deg;    //進行方向に回転
+                bullet_List[i].transform.rotation = Quaternion.AngleAxis(dirVelocity, new Vector3(0, 0, 1));
+            }
+            yield return new WaitForSeconds(0.015f);
+        }
+    }
+
 }
